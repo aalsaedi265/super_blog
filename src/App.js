d2a22331fced7988d88c90fad4e4c01e4f62c9ae
aalsaedi265 <aalsaedi265@gmail.com>
2023-01-26 09:12:11 -0600

    fixed the outelt prblems

--- src/App.js
+++ src/App.js
@@ -2,18 +2,58 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  Route
+  Route,
+  Outlet
 } from "react-router-dom";
+import Login from "./pages/Login";
+import Regester from "./pages/Regester";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Single from "./pages/Single"; 
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet/>
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element: <div>This is the way</div>
+    element: <Layout/>,
+    children:[
+     
+      {
+        path:'/post',
+        element:<Single/>
+      },
+      {
+        path:'/write',
+        element:<Write/>
+      },
+      {
+        path:"/",
+        element: <Home/>
+      },
+
+    ]
   },
   {
-    path:'/test',
-    element: <div>Testing</div>
-  }
+    path:'/regester',
+    element: <Regester/>
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  },
+  
 ])
 
 function App() {

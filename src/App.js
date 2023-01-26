85ea5c94fb90b0b5dd6bc1bead0fe7526141046a
aalsaedi265 <aalsaedi265@gmail.com>
2023-01-26 07:49:03 -0600

    new router set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <div>This is the way</div>
+  },
+  {
+    path:'/test',
+    element: <div>Testing</div>
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}/>
     </div>
   );
 }
---
+++ src/pages/Regester.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Regester (){
+
+    return (
+
+        <div>Regester</div>
+    )
+}
+
+export default Regester

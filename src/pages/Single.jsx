7afb5159a936b2f064cd7c64fc6ffab0cb839c3e
aalsaedi265 <aalsaedi265@gmail.com>
2023-02-09 11:43:33 -0600

    user info

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Single() {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className='content'>
+        <img src="https://www.scuba.com/blog/wp-content/uploads/2020/04/shutterstock_46566328.jpg"/>
+        
+        <div className='user'>
+          <img src="https://www.scuba.com/blog/wp-content/uploads/2020/04/shutterstock_46566328.jpg"/>
+           
+        <div className='info'>
+          <span>Momonga</span>
+          <p>posted 2 days ago</p>
+        </div>
+        <div className='edit'>
+          <Link to="/write?edit=2">
+            <button>EDIT</button>
+          </Link>
+          <button>DELETE</button>
+        </div>
+
+        </div>
+      </div>
+      <div className='menue'>m</div>
+    </div>
   )
 }
 

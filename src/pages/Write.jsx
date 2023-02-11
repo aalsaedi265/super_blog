8481844ce4993a68b3c4d815b185142be12a5a25
aalsaedi265 <aalsaedi265@gmail.com>
2023-02-11 07:32:51 -0600

    created the raido inpute feild

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -24,10 +24,34 @@ function Write() {
           <span>
             <b>Visibility: </b> Public
           </span>
-          <input style={{display:'none'}} type="file" id="file" name=""/>
-          <label htmlFor='file'>UPLOAD ME</label>
+          <input style={{display:'none'}} type="file" id="file" />
+          <label htmlFor='file'>UPLOAD IMG</label>
+          <div className='buttons'>
+            <button>Save as a Draft</button>
+            <button>Update</button>
+          </div>
         </div>
-        <div className="item">i2</div>
+        <div className="item">
+          <h1>Caregory</h1>
+          <input type="radio" name="category" value="art" id="art"/>
+          <label htmlFor="art">Art</label>
+
+          <input type="radio" name="category" value="science" id="science"/>
+          <label htmlFor="science">Science</label>
+
+          <input type="radio" name="category" value="technology" id="technology"/>
+          <label htmlFor="technology">Technology</label>
+
+          <input type="radio" name="category" value="cinema" id="cinema"/>
+          <label htmlFor="cinema">Cinema</label>
+
+          <input type="radio" name="category" value="design" id="design"/>
+          <label htmlFor="dsign">Design</label>
+
+          <input type="radio" name="category" value="food" id="food"/>
+          <label htmlFor="food">Food</label>
+
+        </div>
       </dvi>
     </div>
   )

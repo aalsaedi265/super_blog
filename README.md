97fe319af332e733a57a1389decd68984abadce4
aalsaedi265 <aalsaedi265@gmail.com>
2023-02-11 06:43:24 -0600

    creating the wite component with react quill for better blog text box

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 
 will use react router dom  
+will use react quill to better text
 
 tech in here is mysql, react, node
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 
 function Write() {
+
+  const [value, setValue] = useState('');
+
   return (
-    <div>Write</div>
+    <div className='add'>
+      <div className='content'>
+        <input type='text' placeholder='Title'/>
+        <div className='editorContainer'>
+          <ReactQuill theme="snow" value={value} onChange={setValue} />;
+        </div>
+      </div>
+      <dvi className="menu">
+        <div className="item">i1</div>
+        <div className="item">i2</div>
+      </dvi>
+    </div>
   )
 }
 
